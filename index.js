4f8b4c68626d2ca497a9bec77146c6155121c87c
Ian Pollack <ianjpollack@gmail.com>
2018-02-24 01:09:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+function getFirstSelector(selector) {
+  var firstThing = document.querySelector(selector);
+  return firstThing
+}
+
+function nestedTarget() {
+  var superNest = document.querySelector('div.target');
+  return superNest
+}
