69763af71fa6d7fa42c7bf612476421f68c70b59
Ian Pollack <ianjpollack@gmail.com>
2018-02-24 06:36:17 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ function nestedTarget() {
   var superNest = document.querySelector('div.target');
   return superNest
 }
+
+function increaseRankBy(n) {
+  var m = parseInt(n);
+  const hope = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  const hopeArray = Array.from(hope);
+  const hopeArrayNumbers = parseInt(hopeArray);
+  for (let i = 0; i < hopeArray.length; i++) {
+    hopeArray[i].innerHTML = (i + m).toString();
+  }
+  return hopeArray;
+}
+
+function deepestChild() {
+  const boo = document.querySelector('div#grand-node div div div div');
+  return boo;
+}
