0fed9c512ddfb7bc24f21e8203f1cd07575c95fc
Ian Pollack <ianjpollack@gmail.com>
2018-02-25 04:38:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function increaseRankBy(n) {
     var temp = hopeArray[i].innerHTML;
     var tempNum = parseInt(temp);
     var finalTempNum = (tempNum + m);
-    hopeArray[i].innerHTML = finalTempNum;
+    
   }
 }
 
